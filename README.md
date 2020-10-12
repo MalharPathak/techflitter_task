937bda7e48aa12cda355925dc3f72a35990a3fdf
MalharPathak <malhaar.mp@gmail.com>
2020-10-12 09:53:28 +0530

    Final Commit

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 2 : Provide sorting mechanisms based on at least 2 fields.
 3 : Provide the ability to Create & Delete users
 ```
+
+## Tab 2 - Metadata
+```
+Show metadata about the data / operations from the first tab that you feel is interesting / useful. 
+```
+
+## Comment
+ Comment is written in comment.txt file
--- app/src/main/java/com/android/codingtest/Metadata.java
+++ app/src/main/java/com/android/codingtest/Metadata.java
@@ -7,10 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Metadata extends Fragment {
 View view;
+    String DispUrl="https://gorest.co.in/public-api/users";
+String user,page,current,limit_user;
+TextView total_user,total_page,current_page,limit;
     public Metadata() {
         // Required empty public constructor
     }
@@ -20,6 +31,46 @@ View view;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_metadata, container, false);
+        total_user=view.findViewById(R.id.total_user);
+        total_page=view.findViewById(R.id.total_pages);
+        current_page=view.findViewById(R.id.current_page);
+        limit=view.findViewById(R.id.user_limit);
+        displayList();
         return view;
+    }
+
+    private void displayList() {
+        final VolleyResponse dispApi = new VolleyResponse(getContext());
+        dispApi.executeRequest(Request.Method.GET, DispUrl, new VolleyResponse.VolleyCallback() {
+            @Override
+            public void getResponse(String response) {
+                try {
+                    // Toast.makeText(getContext(), ""+response, Toast.LENGTH_SHORT).show();
+                    JSONObject resp= new JSONObject(response);
+                    String code=resp.getString("code");
+                    if(code.equals("200")) {
+                    JSONObject meta=resp.getJSONObject("meta");
+                    JSONObject pagination=meta.getJSONObject("pagination");
+                    user=pagination.getString("total");
+                    page=pagination.getString("pages");
+                    current=pagination.getString("page");
+                    limit_user=pagination.getString("limit");
+
+                    total_user.setText("Total User:"+user);
+                    total_page.setText("Total Pages: "+page);
+                    current_page.setText("Current Page : "+current);
+                    limit.setText("Page Limit: "+limit_user);
+
+                    }else{
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 }
